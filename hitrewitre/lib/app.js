bf4d7ce0694360119fb6ede39496fbf75320f754
Imre Mehesz <imehesz@imehesz-lenny.(none)>
2012-11-04 21:13:10 -0500

    added card handling and money tracking

--- hitrewitre/lib/app.js
+++ hitrewitre/lib/app.js
@@ -2,12 +2,45 @@
 (function() {
   var app;
 
-  app = angular.module('twentyone', []);
+  Array.prototype.shuffle = function() {
+    return this.sort(function() {
+      return 0.5 - Math.random();
+    });
+  };
+
+  app = angular.module('hitrewitre', []);
 
   app.controller("GameCtrl", function($scope) {
-    return $scope.demo = "It is working, just need to manually bootstrap the app";
+    $scope.money = 50;
+    $scope.wager = 10;
+    $scope.cards = [
+      {
+        "name": "Q Spade",
+        "points": 0
+      }, {
+        "name": "Q Hearts",
+        "points": 1
+      }, {
+        "name": "Q Clubs",
+        "points": 0
+      }
+    ].shuffle();
+    $scope.pickCard = function() {
+      if (this.card.points > 0) {
+        $scope.money += $scope.wager;
+      } else {
+        $scope.money -= $scope.wager;
+      }
+      if ($scope.money < 0) {
+        $scope.money = 0;
+      }
+      return $scope.cards.shuffle();
+    };
+    return $scope.drawCard = function() {
+      return this;
+    };
   });
 
-  angular.bootstrap(document, ["twentyone"]);
+  angular.bootstrap(document, ["hitrewitre"]);
 
 }).call(this);
--- hitrewitre/src/app.coffee
+++ hitrewitre/src/app.coffee
@@ -1,6 +1,25 @@
-app = angular.module 'twentyone', []
+Array::shuffle = -> @sort -> 0.5 - Math.random()
+
+app = angular.module 'hitrewitre', []
 
 app.controller "GameCtrl", ($scope) ->
-  $scope.demo = "It is working, just need to manually bootstrap the app"
+  $scope.money = 50
+  $scope.wager = 10
 
-angular.bootstrap document, ["twentyone"]
+  $scope.cards = [ {"name":"Q Spade","points":0},{"name":"Q Hearts","points":1},{"name":"Q Clubs","points":0} ].shuffle()
+
+  $scope.pickCard = ->
+    if @.card.points > 0
+      $scope.money += $scope.wager
+    else
+      $scope.money -= $scope.wager
+
+    if $scope.money < 0
+      $scope.money = 0
+
+    $scope.cards.shuffle()
+
+  $scope.drawCard = ->
+    @
+
+angular.bootstrap document, ["hitrewitre"]
